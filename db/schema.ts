54d4743d3188eda34ca3a4fa872d640f9a6ccad3
mrx7355608 <mrx7355608@gmail.com>
2024-12-11 00:38:56 +0500

    created new scheam for tracking expenses

--- db/schema.ts
+++ db/schema.ts
@@ -10,3 +10,14 @@ export const recordsSchema = sqliteTable(
     },
     (_table) => [],
 );
+
+export const expensesSchema = sqliteTable(
+    "expenses",
+    {
+        id: int().primaryKey({ autoIncrement: true }),
+        item: text().notNull(),
+        amount_spent: int().notNull(),
+        record: int("record_id").references(() => recordsSchema.id),
+    },
+    (table) => [index("record_idx").on(table.record)],
+);
