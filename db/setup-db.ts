823c3dc98de79437489478eab8ce87ab7b0a34c3
mrx7355608 <mrx7355608@gmail.com>
2024-12-18 01:27:04 +0500

    introduced a cached connection in setup-db function

--- db/setup-db.ts
+++ db/setup-db.ts
@@ -1,7 +1,13 @@
 import * as Sqlite from "expo-sqlite";
-import { drizzle } from "drizzle-orm/expo-sqlite";
+import * as schema from "./schema";
+import { drizzle, ExpoSQLiteDatabase } from "drizzle-orm/expo-sqlite";
 
-export const setupDb = () => {
+let cachedDb: ExpoSQLiteDatabase<typeof schema> | null = null;
+
+export const setupDb = (): ExpoSQLiteDatabase<typeof schema> => {
+    if (cachedDb) {
+        return cachedDb;
+    }
     const expo = Sqlite.openDatabaseSync("mydb.db");
     expo.execSync(
         "CREATE TABLE IF NOT EXISTS expenses (id integer PRIMARY KEY AUTOINCREMENT NOT NULL,item text NOT NULL,amount_spent integer NOT NULL,record_id integer,date text DEFAULT (CURRENT_DATE),FOREIGN KEY (`record_id`) REFERENCES `records`(`id`) ON UPDATE no action ON DELETE no action);",
@@ -13,6 +19,8 @@ export const setupDb = () => {
         "CREATE TABLE IF NOT EXISTS records (id integer PRIMARY KEY AUTOINCREMENT NOT NULL, image text NOT NULL, bought_price INTEGER NOT NULL, sold_price INTEGER DEFAULT 0);",
     );
 
-    const db = drizzle(expo);
+    const db = drizzle(expo, { schema });
+    cachedDb = db;
+
     return db;
 };
